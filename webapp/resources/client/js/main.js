0ebb1044dd236a1e1c8bc6d746fc6f0e44c19fdf
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2025-03-13 21:13:26 +0700

    fix bug carsel ok

--- webapp/resources/client/js/main.js
+++ webapp/resources/client/js/main.js
@@ -265,43 +265,6 @@
     })();
 
 
-    // Testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav: true,
-        navText: [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            576: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            992: {
-                items: 2
-            },
-            1200: {
-                items: 2
-            }
-        }
-    });
-
-
-
-
-
 
 
     // Product Quantity
@@ -816,4 +779,17 @@ $(document).ready(function () {
 
 
 
-//
+//admin and hero
+$(document).ready(function () {
+    $('#heroCarousel').carousel({
+        interval: 3000, // Chuyển slide sau 3 giây
+        pause: 'hover'  // Tạm dừng khi hover
+    });
+});
+
+$(document).ready(function () {
+    $('#adminCarousel').carousel({
+        interval: 4000, // Chuyển slide sau 4 giây
+        pause: 'hover'  // Tạm dừng khi hover
+    });
+});
